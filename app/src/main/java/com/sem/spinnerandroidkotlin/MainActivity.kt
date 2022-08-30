f8c3b82396aa47f3c748776fc46cb8ded60c34a2
Nikolui333 <56168199+Nikolui333@users.noreply.github.com>
2022-08-30 12:00:38 +0300

    app starts but spinner is empty

--- app/src/main/java/com/sem/spinnerandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/sem/spinnerandroidkotlin/MainActivity.kt
@@ -2,10 +2,40 @@ package com.sem.spinnerandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.sem.spinnerandroidkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    private var option : Spinner? = null
+    private var result : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val array = arrayOf("Option 1", "Option 2", "Option 3")
+
+        option?.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array)
+
+        option?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                result?.text = "Please Select an Option"
+            }
+
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                result?.text = array.get(0)
+            }
+
+        }
     }
 }
